043d850be77b8fb844c0e63e8d1050e5c29d50df
Roberto Rodriguez <roberto@rodriguezartavia.com>
2014-03-24 08:39:27 -0600

    add file to test external script

---
+++ dist/sf_test.js
@@ -0,0 +1,7 @@
+Visualforce.remoting.Manager.invokeAction(
+"{!$RemoteAction.ThreeVotApiController.handleRest}", "post", "Account", '{"Name":"Rodco2"}',
+function(result, event){
+  console.log(result);
+}, 
+{escape: true}
+);
