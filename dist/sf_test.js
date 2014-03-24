73580b9582c580cb857105949270f33d27bbc757
Roberto Rodriguez <roberto@rodriguezartavia.com>
2014-03-24 08:44:18 -0600

    trying VF Remoring with Alternate Call

--- dist/sf_test.js
+++ dist/sf_test.js
@@ -1,7 +1,9 @@
-Visualforce.remoting.Manager.invokeAction(
-"{!$RemoteAction.ThreeVotApiController.handleRest}", "post", "Account", '{"Name":"Rodco2"}',
-function(result, event){
-  console.log(result);
-}, 
-{escape: true}
+ThreeVotApiController.handleRest( 
+  "post", 
+  "Account", 
+  '{"Name":"Rodco2"}', 
+  function(result, event){ 
+    console.log(result); 
+  }, 
+  { escape: true }
 );
